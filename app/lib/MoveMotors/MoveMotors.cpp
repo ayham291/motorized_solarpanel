5049017794851ee9a4ceadb6bcf97e247955d2a6
ayham291 <ayham.dev@ayhamcloud.de>
2021-05-27 12:30:27 +0200

    fix MoveMotors
    - swith LOW and HIGH for powering Motors
    - make bunch of defines for the code to make sense

--- app/lib/MoveMotors/MoveMotors.cpp
+++ app/lib/MoveMotors/MoveMotors.cpp
@@ -1,47 +1,46 @@
 #include <MoveMotors.h>
-#include <Arduino.h>
 
 void Motor::init_motor(uint8_t pwr_pin, uint8_t dir_pin, int pos)
 {
     pinMode(pwr_pin, OUTPUT);
-    digitalWrite(pwr_pin, LOW); // initial state as off
+    digitalWrite(pwr_pin, HIGH); // initial state as off
     this->dir_pin=dir_pin;
     this->pwr_pin=pwr_pin;
     this->pos=pos;
     this->pwr=0;
-    this->rot="NaN"; // means error ie no rotation
+    this->rot=NXN; // means error ie no rotation
 }
 // switch motor power on and off
-void Motor::switch_pwr(char pwr)
+void Motor::switch_pwr(int pwr)
 {
-   /* TO BE IMPLEMENTED:
-        - TIMED SWITCH
-        - DEGREES -> SECONDS
-   */
-    if(pwr=="on")
-    {
-        digitalWrite(pwr_pin, HIGH);
-        this->pwr = 1;
-    }
-    if(pwr=="off")
+    if(pwr==ON)
     {
         digitalWrite(pwr_pin, LOW);
+        this->pwr = 1;
+    }
+    if(pwr==OFF)
+    {
+        digitalWrite(pwr_pin, HIGH);
         this->pwr = 0;
     }
 }
 void Motor::rotate(int dir)
 {
-    if(dir==0)
+    if(dir==RIGHT)
     {
         pinMode(this->dir_pin, OUTPUT);
         digitalWrite(this->dir_pin, LOW);
-        this->rot = "Left";
+        this->rot = RIGHT;
+        this->dir = UP;
+        switch_pwr(ON);
     }
-    if(dir==1)
+    if(dir==LEFT)
     {
         pinMode(this->dir_pin, OUTPUT);
         digitalWrite(this->dir_pin, HIGH);
-        this->rot = "Right";
+        this->rot = LEFT;
+        this->dir = DOWN;
+        switch_pwr(ON);
     }
 
 /*     switch(dir) */
--- app/lib/MoveMotors/MoveMotors.h
+++ app/lib/MoveMotors/MoveMotors.h
@@ -1,21 +1,34 @@
 #ifndef __MOVE_
 #define __MOVE_
+#include <Arduino.h>
+#include <EndPos.h>
+
+#define NXN 3
+#define ON 1
+#define OFF 0
+#define LEFT 0
+#define RIGHT 1
+#define UP 1
+#define DOWN 0
+
 
 class Motor
 {
 private:
     int pwr_pin;
     int dir_pin;
+    int dir;
     int pos;
     int pwr;
-    char rot;
+    int rot;
+    EndPos endPos;
 public:
     void init_motor(uint8_t pwr_pin, uint8_t dir_pin, int pos);
-    void switch_pwr(char pwr);
+    void switch_pwr(int pwr);
     void rotate(int dir);
     int get_pwr();
     int get_dir();
-    int get_rot();
+    char get_rot();
 };
 
 #endif
