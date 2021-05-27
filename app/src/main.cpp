9263082d573f2053325749e17d38331ad792f6b1
ayham291 <ayham.dev@ayhamcloud.de>
2021-05-27 12:35:58 +0200

    add main.cpp with test code
    - [x] turn table to find sun
    - [x] get azimuth and time

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,12 +1,67 @@
 #include <Arduino.h>
+#include <EndPos.h>
+#include <MoveMotors.h>
+#include <SunPosition.h>
+#include <RTClib.h>
+#include <Wire.h>
+#include <FindSun.h>
 
+EndPos endPos;
+Motor turnTable;
+Motor tiltPanel;
+SonnenStand sunpos;
+Find_Sun FindSun;
 void setup()
 {
-
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  endPos.init_sensors(D6, D5);
+  turnTable.init_motor(D0, D3, 1);
+  tiltPanel.init_motor(D7, D4, 2);
+  sunpos.init_time();
+  FindSun.init_compass();
+  pinMode(A0, INPUT);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  FindSun.get_current_azimuth();
+  sunpos.get_azimuth(sunpos.get_arr_pos());
+  if(FindSun.offest_to_sun()<-2)
+    turnTable.rotate(RIGHT);
+  else if(FindSun.offest_to_sun()>2)
+    turnTable.rotate(LEFT);
+  else if (FindSun.offest_to_sun()>=2 || FindSun.offest_to_sun()>=-2)
+    turnTable.switch_pwr(OFF);
+  // Serial.print("KOMPASS WERT: ");
+  // Serial.println(FindSun.get_current_azimuth());
+  // delay(1000);
+
+  // Serial.print("SOllWERT: ");
+  // Serial.println(sunpos.get_azimuth(sunpos.get_arr_pos()));
+  // delay(1000);
+
+  // Serial.print("OFFSET: ");
+  // Serial.println(FindSun.offest_to_sun());
+  // delay(1000);
+
+  
+  Serial.println(analogRead(A0));
+
+
+  // endPos.read_pins();
+  // if (endPos.getPosPhiUp() == 1 && endPos.getPosPhiDown() == 1)
+  // {
+  //   tiltPanel.switch_pwr(OFF);
+  //   tiltPanel.rotate(DOWN); // auch on
+  // }
+  // else if (endPos.getPosPhiDown() == 0 && endPos.getPosPhiUp() == 0)
+  // {
+  //   tiltPanel.switch_pwr(OFF);
+  //   tiltPanel.rotate(UP);
+  // }
+  // else
+  // {
+  //   tiltPanel.switch_pwr(OFF);
+  // }
+  // Serial.println(sunpos.get_azimuth(sunpos.get_arr_pos()));
 }
