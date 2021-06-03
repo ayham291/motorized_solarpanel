368e301c49211ee5329d34e2b8f39626e6208beb
ayham291 <ayham.dev@ayhamcloud.de>
2021-06-03 21:08:01 +0200

    bug fixes in main.cpp

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -6,25 +6,26 @@
 #include <Wire.h>
 #include <FindSun.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PCF8591.h>
 
 char ssid[] = "ADD_SSID";
 char pass[] = "ADD_PASSWORD";
 int status = WL_IDLE_STATUS;
-int check_rotation=1;
+int check_rotation = 1;
 char server[] = "arduino.ayhamcloud.de";
-String postData_k;
-String postData_d;
-String postData;
 
-WiFiClient client;
+String postData;
+String endpos_val;
+
 int ticks = 0;
 int gen_status = 1;
-String endpos_val;
 int az;
 float diode;
 int offset;
 float temperature;
 
+WiFiClientSecure client;
 RTC_DS3231 rtc_temp_only;
 EndPos endPos;
 Motor turnTable;
@@ -32,13 +33,13 @@ Motor tiltPanel;
 SonnenStand sunpos;
 Find_Sun FindSun;
 
-
 void setup()
 {
   Serial.begin(9600);
   rtc_temp_only.begin();
   turnTable.init_motor(D0, D3, 1);
   tiltPanel.init_motor(D7, D4, 2);
+  endPos.init_sensors(D6, D5);
   FindSun.init_compass();
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED)
@@ -65,31 +66,32 @@ void loop()
   offset = FindSun.offset_to_Sun();
   temperature = rtc_temp_only.getTemperature();
 
+  check_rotation = FindSun.check_rotation();
 
-  check_rotation=FindSun.check_rotation();
-
-  if(check_rotation==0)
+  if (check_rotation == 0)
     FindSun.check_tilt();
-  
-  if(ticks==5)
+
+  if (ticks == 5)
   {
-    if(endPos.getPosPhiDown()==0)
+    endPos.read_pins();
+    if (endPos.getPosPhiDown() == 0 && endPos.getPosPhiUp() == 0)
       endpos_val = "Down";
-    else if(endPos.getPosPhiUp()==1)
+    else if (endPos.getPosPhiUp() == 1 && endPos.getPosPhiDown() == 1)
       endpos_val = "Up";
     else
       endpos_val = "Tilted";
-    
+
     postData = "kompass=" + (String)az;
     postData += "&diode=" + (String)diode;
     postData += "&offset=" + (String)offset;
-    postData += "&temperature" + (String)temperature;
+    postData += "&temp" + (String)temperature;
     postData += "&endpost=" + (String)endpos_val;
     postData += "&status=" + (String)gen_status;
     postData += "&tiltPanel=" + (String)tiltPanel.get_rot();
     postData += "&turnTable=" + (String)turnTable.get_rot();
-    
-    if (client.connect(server, 80))
+    postData += "&sunpos=" + (String)sunpos.get_azimuth(sunpos.get_arr_pos());
+
+    if (client.connect(server, 443))
     {
       client.println("POST / HTTP/1.1");
       client.println("Content-Type: application/x-www-form-urlencoded");
