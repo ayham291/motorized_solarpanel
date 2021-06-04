15b87e77172025acdb206bea343e04945b27b0be
ayham291 <ayham.dev@ayhamcloud.de>
2021-06-04 23:51:25 +0200

    change FindSun -> new check_tilt: LDR_Offset and AD_Converter

--- app/lib/FindSun/FindSun.h
+++ app/lib/FindSun/FindSun.h
@@ -7,9 +7,9 @@
 #include <SunPosition.h>
 #include <MoveMotors.h>
 #include <EndPos.h>
+#include <PCF8591.h>
 
 #define TOLERANCE 3
-
 
 #define Mode_Standby 0b00000000
 #define Mode_Continuous 0b00000001
@@ -35,10 +35,12 @@ private:
     int Sun_at_azimuth;
     int current_azimuth;
     int offset;
+    int offset_adc;
+    int ldr0;
+    int ldr1;
     Motor tiltpanel;
     Motor turnTable;
     EndPos endPos;
-
 public:
     void init_compass();
     int get_current_azimuth();
@@ -46,6 +48,8 @@ public:
     void check_tilt();
     int check_rotation();
     void start_pos();
+    int get_ldr0();
+    int get_ldr1();
 };
 
 #endif
