bc799d5346b9ffc71353cdcfa5f0a18a99e5e279
Ayham <69654717+ayham291@users.noreply.github.com>
2021-06-06 19:19:53 +0200

    update main.cpp -> abgabe code

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -5,19 +5,15 @@
 
 int check_rotation = 1;
 int ticks = 0;
+
 Find_Sun FindSun;
 Wireless client;
-Motor turnTable;
-Motor tiltPanel;
 
 void setup()
 {
   Serial.begin(9600);
-  turnTable.init_motor(D0, D3, 1);
-  tiltPanel.init_motor(D7, D4, 2);
   FindSun.init_compass();
   client.Wifi_setup();
-  // FindSun.start_pos();
 }
 
 void loop()
